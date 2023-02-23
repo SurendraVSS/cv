3c23a8ceb63af2231a53261e0dd70568c7ac0cd4
SurendraVSS <106951672+SurendraVSS@users.noreply.github.com>
2023-02-23 16:49:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-//app.use('/', express.static('./dist/tata-cv-online/browser'))//set the static path 
-//app.set('view engine', 'pug');
+app.use('/', express.static('./dist/tata-cv-online/browser'))//set the static path 
+app.set('view engine', 'pug');
 
 
 
